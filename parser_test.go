f4cda0c7ef6b042c5b842415ad71c3c4cabe11df
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2020-01-19 14:09:26 +0500

    add empty TestGetDiff

---
+++ parser_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestGetDiff(t *testing.T) {
+	// cases
+	// add new file
+	// delete old file
+	// update new file
+	// should get line numbers by file and namespace
+	t.Fatal("not implemented")
+}
