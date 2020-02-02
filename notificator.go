eda40208f833152d6bd0ab4c90e5443803671595
Mirolim Mirzakhmedov <mirolim.mirzakhmedov@gmail.com>
2020-02-02 17:37:28 +0500

    display notification on darwin os

--- notificator.go
+++ notificator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"runtime"
 	"strconv"
 )
 
@@ -31,11 +32,22 @@ func (n *DesktopNotificator) Run(ctx context.Context) (string, error) {
 }
 
 func (n *DesktopNotificator) Send(ctx context.Context, msg string) error {
-	cmd := exec.CommandContext(ctx, "notify-send", "-t", n.expireTime)
-	if n.transient {
-		cmd.Args = append(cmd.Args, []string{"--hint", "int:transient:1"}...)
+	var cmd *exec.Cmd
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = exec.CommandContext(ctx, "osascript", "-e",
+			fmt.Sprintf("'display notification \"%s\" with title \"%s\"'", msg, msg))
+
+	case "linux":
+		cmd = exec.CommandContext(ctx, "notify-send", "-t", n.expireTime)
+		if n.transient {
+			cmd.Args = append(cmd.Args, []string{"--hint", "int:transient:1"}...)
+		}
+		cmd.Args = append(cmd.Args, msg)
+	default:
+		return fmt.Errorf("unsupported os %s", runtime.GOOS)
 	}
-	cmd.Args = append(cmd.Args, msg)
+
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("Desktop notification error %v", err)
